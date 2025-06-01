ab1bdd938d3c5c2296e33971a8f962f16d2a586a
nstranges <nicholastranges4@hotmail.com>
2025-06-01 10:25:34 -0400

    Increased the time of the eval harness.

--- Benchmarking/eval-harness-job.sh
+++ Benchmarking/eval-harness-job.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --account=def-lenck
 #SBATCH --export=ALL,DISABLE_DCGM=1
-#SBATCH --time=10:00:00
+#SBATCH --time=20:00:00
 #SBATCH --gpus=1
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
@@ -28,6 +28,9 @@ cd /lustre07/scratch/nstrang2/Datasets/lm-evaluation-harness/
 pip install --no-index --upgrade pip
 pip install /home/nstrang2/scratch/Libraries/word2number-1.1.zip
 pip install --no-index langdetect
+pip install --no-index immutabledict
+pip install --no-index nltk
+pip install --no-index packaging
 pip install --no-index -e .[dev]
 
 export PYTORCH_CUDA_ALLOC_CONF="expandable_segments:True"
